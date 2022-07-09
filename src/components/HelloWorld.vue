1a0ac6ad55c871a88affb490c15ece8abb3f731e
MMC <mc.christiansen@outlook.com>
2022-07-09 19:10:41 +0200

    Changed to use correct implementation of i18n in components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,30 @@
 <template>
-<h1>{{test}}</h1>
-<h2>{{$t(TranslationKeys.Message)}}</h2>
+  <h1>{{ message }}</h1>
+  <h2>{{ $t(TranslationKeys.Message) }}</h2>
+  <v-btn color="blue-grey" style="color: white !important" @click="changeLanguage">
+    Change language
+  </v-btn>
 </template>
 
 <script setup lang='ts'>
-import { TranslationKeys } from '@/resources/translationKeys'
+import { LanguageType } from '@/resources/languageTypes';
+import { TranslationKeys } from '@/resources/translationKeys';
+import { useRootStore } from '@/store/root/rootStore';
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+
 interface Props {
   test: string
+} defineProps<Props>();
+
+
+const store = useRootStore();
+const { t } = useI18n();
+
+
+
+var message = computed(() => t(TranslationKeys.Message))
+function changeLanguage() {
+  store.SetLanguage(store.GetCurrentLanguage == LanguageType.DK ? LanguageType.EN : LanguageType.DK);
 }
-defineProps<Props>()
 </script>

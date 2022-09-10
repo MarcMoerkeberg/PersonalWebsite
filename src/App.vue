2a7525ba5cbb137494b7414d8c83bd73686c9428
MMC <mc.christiansen@outlook.com>
2022-09-10 13:47:47 +0200

    idk, some vuetify title stuff

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="grid">
-    <div class="content">
-      <v-app>
-        <v-main>
-          <router-view />
-        </v-main>
-      </v-app>
-    </div>
-  </div>
+	<v-app>
+		<v-app-bar app
+				   color="primary"
+				   dark>
+			<v-toolbar-title>Her skal der være en lækker titel!</v-toolbar-title>
+		</v-app-bar>
+		<v-main color="primary"
+				dark>
+			<router-view />
+		</v-main>
+	</v-app>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style>
-
 </style>

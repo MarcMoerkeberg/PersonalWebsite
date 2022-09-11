d353f6a32a9e0c03e161eda5b135d5c1a5937a2d
MMC <mc.christiansen@outlook.com>
2022-09-11 20:53:38 +0200

    Hide app-bar when routerview is landing-page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 	<v-app>
 		<v-app-bar app
 				   color="primary"
-				   dark>
+				   dark
+				   v-if="showAppBar">
 			<v-toolbar-title>Her skal der være en lækker titel!</v-toolbar-title>
 		</v-app-bar>
 		<v-main color="primary"
@@ -13,7 +14,15 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+var routesToHideAppBar = ['landing-page'];
+var currentRoute = computed(() => { return useRoute().name });
+var showAppBar = computed(() => { return currentRoute.value && !routesToHideAppBar.includes(currentRoute.value.toString()) });
+
 </script>
 
 <style>
+
 </style>

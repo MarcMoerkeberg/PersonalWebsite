b4eb9648f11a731ba9948bd3eafc43e93531609f
MC <mch@e-komplet.dk>
2022-11-12 19:11:11 +0100

    Redirectionbox component styling, props etc

--- src/components/landingPage/RedirectionBox.vue
+++ src/components/landingPage/RedirectionBox.vue
@@ -1,22 +1,37 @@
 <script setup lang="ts">
+import { RoutePathEnum } from '@/models/enums/RoutePathEnum';
+import { baseStore } from '@/store/base/baseStore';
+import { computed } from 'vue';
+
 interface Props {
     headLine: string,
     summaryText: string,
-    imagePath: string
+    imagePath: string,
+    nagivationPath: RoutePathEnum,
+    summaryHeightPercent?: number
 }
+const componentProps = defineProps<Props>();
 
-const props = defineProps<Props>();
+const store = baseStore();
+const summaryHeight = computed(() => componentProps.summaryHeightPercent === undefined ? '40%' : `${componentProps.summaryHeightPercent}%;`).value;
 </script>
 
 <template>
-    <v-card class="margin-x-20p width-20vw">
-        <v-img class="align-items-center"
-               :src="props.imagePath">
-            <v-card-title class="text-center text-xl-h4">{{ props.headLine }}</v-card-title>
-        </v-img>
-    </v-card>
+    <v-hover v-slot="{ isHovering, props }">
+        <v-card :class="['margin-x-20p', 'width-20vw', { 'cursor-pointer': isHovering }]"
+                v-bind="props"
+                @click="store.navigateToView(componentProps.nagivationPath)">
+            <v-img class="align-items-center"
+                   :src="componentProps.imagePath">
+                <v-card-title class="text-center text-xl-h4">{{ componentProps.headLine }}</v-card-title>
+                <v-expand-transition>
+                    <div v-if="isHovering"
+                         class="d-flex transition-fast-in-fast-out v-card-overlay"
+                         :style="{ height: summaryHeight }">
+                        {{ componentProps.summaryText }}
+                    </div>
+                </v-expand-transition>
+            </v-img>
+        </v-card>
+    </v-hover>
 </template>
-
-<style scoped>
-
-</style>

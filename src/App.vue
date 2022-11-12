a90f59fbf28eae24dea0243ae7415da916a70738
MC <mch@e-komplet.dk>
2022-11-12 19:10:36 +0100

    App routerview height and state route updating

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
+<script setup lang="ts">
+import { computed, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { baseStore } from './store/base/baseStore';
+
+const store = baseStore();
+var currentRoute = computed(() => { return useRoute() }).value;
+
+watch(currentRoute, (newVal) => { store.setCurrentRoute(newVal); })
+</script>
+		
 <template>
 	<v-app>
 		<v-app-bar app
 				   color="primary"
 				   dark
-				   v-if="showAppBar">
+				   v-if="store.showAppBar">
 			<v-toolbar-title>Her skal der være en lækker titel!</v-toolbar-title>
 		</v-app-bar>
 		<v-main color="primary"
 				dark>
-			<router-view />
+			<router-view style="height: 100%;" />
 		</v-main>
 	</v-app>
 </template>
-
-<script setup lang="ts">
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-import { baseStore } from './store/base/baseStore';
-
-const store = baseStore();
-
-var currentRoute = computed(() => { return useRoute() });
-var showAppBar = computed(() => { return !store.RoutesToHideAppBar.includes(currentRoute.value.path); })
-</script>

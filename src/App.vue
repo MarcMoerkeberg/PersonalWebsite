58632a3102b45a2614932d085c03a2d3568be3c2
MC <mch@e-komplet.dk>
2023-03-14 23:01:59 +0100

    Hide scrollbar on all views

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,31 @@ import { useRoute } from 'vue-router';
 import { baseStore } from './store/base/baseStore';
 
 const store = baseStore();
-var currentRoute = computed(() => { return useRoute() }).value;
+var currentRoute = computed(() => { return useRoute() });
 
-watch(currentRoute, (newVal) => { store.setCurrentRoute(newVal); })
+watch(currentRoute.value, (newVal) => { store.setCurrentRoute(newVal); })
 </script>
 		
 <template>
 	<v-app>
 		<v-app-bar app
 				   color="primary"
-				   dark
 				   v-if="store.showAppBar">
+
 			<v-toolbar-title>Her skal der være en lækker titel!</v-toolbar-title>
+
 		</v-app-bar>
-		<v-main color="primary"
-				dark>
+
+		<v-main color="primary">
 			<router-view style="height: 100%;" />
 		</v-main>
 	</v-app>
 </template>
+
+<style>
+html {
+	overflow: hidden !important;
+	/* scrollbar-width: none; */
+	/* -ms-overflow-style: none; */
+}
+</style>

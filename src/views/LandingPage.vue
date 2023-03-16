a8ecf04ab9117cc83c5869e1b75588d7609bab81
MC <mch@e-komplet.dk>
2023-03-16 23:14:27 +0100

    Changed styling and added icons

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,25 +1,40 @@
 <script setup lang="ts">
 import RedirectionBoxVue from '@/components/landingPage/RedirectionBox.vue';
-import { RoutePathEnum } from '@/models/enums/RoutePathEnum';
 import { TranslationKeys } from '@/resources/translationKeys';
 import { useI18n } from 'vue-i18n';
+import { Icon } from '@iconify/vue';
+import { baseStore } from '@/store/base/baseStore';
+import { RoutePath } from '@/models/enums/RoutePath'
+import { ExternalRoute } from '@/models/enums/ExternalRoute'
+import { GetSocialInformation } from '@/models/base/IconInformation';
 
-const importedImage = require('@/assets/images/speederman.png')
+const store = baseStore();
 const { t } = useI18n();
+const iconInformationForSocials = GetSocialInformation();
 </script>
 
 <template>
-    <div class="flex-center gap-50p row">
-        <RedirectionBoxVue :head-line="t(TranslationKeys.About)"
-                           :summary-text="t(TranslationKeys.AboutSummary)"
-                           :nagivation-path="RoutePathEnum.About" />
+      <div class="vertical-center">
+            <div class="d-flex justify-content-center gap-50p">
+                  <RedirectionBoxVue :head-line="t(TranslationKeys.About)"
+                                     :summary-text="t(TranslationKeys.AboutSummary)"
+                                     :nagivation-path="RoutePath.About" />
 
-        <RedirectionBoxVue :head-line="t(TranslationKeys.Blog)"
-                           :summary-text="t(TranslationKeys.BlogSummary)"
-                           :nagivation-path="RoutePathEnum.Blog" />
+                  <RedirectionBoxVue :head-line="t(TranslationKeys.Blog)"
+                                     :summary-text="t(TranslationKeys.BlogSummary)"
+                                     :nagivation-path="RoutePath.Blog" />
 
-        <RedirectionBoxVue :head-line="t(TranslationKeys.Contact)"
-                           :summary-text="t(TranslationKeys.ContactSummary)"
-                           :nagivation-path="RoutePathEnum.Contact" />
-    </div>
+                  <RedirectionBoxVue :head-line="t(TranslationKeys.Contact)"
+                                     :summary-text="t(TranslationKeys.ContactSummary)"
+                                     :nagivation-path="RoutePath.Contact" />
+            </div>
+
+            <div class="flex-center margin-top-10p">
+                  <Icon v-for="socialInfo in iconInformationForSocials"
+                        :icon="socialInfo.Icon"
+                        @click="store.OpenInNewTab(socialInfo.Link)"
+                        class="cursor-pointer margin-x-8p"
+                        width="3em" />
+            </div>
+      </div>
 </template>
